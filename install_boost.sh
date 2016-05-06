2925fb82b231884aadeaeec968726a41d7bb3971
joobog <eugen.betke@gmail.com>
2016-05-06 15:55:31 +0200

    SIOX + SIOX-Deps

--- install_boost.sh
+++ install_boost.sh
@@ -5,72 +5,103 @@ export SCRIPTPATH=`dirname ${SCRIPT}`
 
 ${SCRIPTPATH}/precheck
 
+module purge
+
 export NAME="boost"
 export MAJOR="1"
-export MINOR="54"
+export MINOR="60"
 export REVISION="0"
 export VERSION="${MAJOR}_${MINOR}_${REVISION}"
 
 source ${SCRIPTPATH}/config
+
 SRCdir="${CACHEdir}/${NAME}_${VERSION}" 		# source directory
 
-export BZIP2_INCLUDE="${INSTALL_ROOT_DIR}/bzip2/include"
-export BZIP2_LIBRARY="${INSTALL_ROOT_DIR}/bzip2/lib"
-export BZIP2_BINARY="${INSTALL_ROOT_DIR}/bzip2/bin"
-export BZIP2_SOURCE="${INSTALL_ROOT_DIR}/bzip2/src"
+#export BZIP2_INCLUDE="${INSTALL_ROOT_DIR}/bzip2/include"
+#export BZIP2_LIBRARY="${INSTALL_ROOT_DIR}/bzip2/lib"
+#export BZIP2_BINARY="${INSTALL_ROOT_DIR}/bzip2/bin"
+#export BZIP2_SOURCE="${INSTALL_ROOT_DIR}/bzip2/src"
 
-if [ ! -d ${BZIP2_INCLUDE} ]
-then
-	echo "${BZIP2_INCLUDE} not found"
-	exit 1;
-fi
-if [ ! -d ${BZIP2_LIB} ]
-then
-	echo "${BZIP2_LIB} not found"
-	exit 1;
-fi
-if [ ! -d ${BZIP2_BINARY} ]
-then
-	echo "${BZIP2_BINARY} not found"
-	exit 1;
-fi
-if [ ! -d ${BZIP2_SOURCE} ]
-then
-	echo "${BZIP2_SOURCE} not found"
-	exit 1;
-fi
+#if [ ! -d ${BZIP2_INCLUDE} ]
+#then
+#  echo "${BZIP2_INCLUDE} not found"
+#  exit 1;
+#fi
+#if [ ! -d ${BZIP2_LIB} ]
+#then
+#  echo "${BZIP2_LIB} not found"
+#  exit 1;
+#fi
+#if [ ! -d ${BZIP2_BINARY} ]
+#then
+#  echo "${BZIP2_BINARY} not found"
+#  exit 1;
+#fi
+#if [ ! -d ${BZIP2_SOURCE} ]
+#then
+#  echo "${BZIP2_SOURCE} not found"
+#  exit 1;
+#fi
 
-echo "boost bzlib2 include: ${BZLIB2_INCLUDE_DIR}"
-echo "boost bzlib2 lib: ${BZLIB2_LIBRARY_DIR}"
-echo "boost bzlib2 bin: ${BZLIB2_BINARY_DIR}"
+#echo "boost bzlib2 include: ${BZLIB2_INCLUDE_DIR}"
+#echo "boost bzlib2 lib: ${BZLIB2_LIBRARY_DIR}"
+#echo "boost bzlib2 bin: ${BZLIB2_BINARY_DIR}"
 
 
+FILE="${NAME}_${VERSION}.tar.bz2"
 ### FETCH ###
 cd ${CACHEdir}
-if [ ! -e "${CACHEdir}/${NAME}_${VERSION}" ] 
+if [ ! -f "${FILE}" ] 
 then
-	SOURCE="http://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${REVISION}/${NAME}_${VERSION}.tar.bz2"
-	wget $SOURCE
+	wget "http://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${REVISION}/${FILE}"
 else
-	echo "WARNING: Archive already exists"
+	echo "WARNING: ${FILE} already exists"
 fi
 
 
 ### EXTRACT ###
 if [ ! -d "${SRCdir}" ] 
 then
-	bunzip2 "${CACHEdir}/${NAME}_${VERSION}.tar.bz2"  &>> $LOG
-	tar -xvf "${CACHEdir}/${NAME}_${VERSION}.tar" &>> $LOG
+	tar -xjvf "${CACHEdir}/${NAME}_${VERSION}.tar.bz2"
 else
 	echo "WARNING: A copy of archive is already extracted"
 fi
 
 ### COMPILE AND INSTALL ###
 cd ${SRCdir}
-${SRCdir}/bootstrap.sh  --prefix=$PREFIX  &>> $LOG
-${SRCdir}/b2 -j${THREAD_NUM}  &>> $LOG
-${SRCdir}/b2 -j${THREAD_NUM} --build-type=complete --layout=tagged  --prefix=${PREFIX} install &>> $LOG
+${SRCdir}/bootstrap.sh  --prefix=$PREFIX
+${SRCdir}/b2 -j${THREAD_NUM}
+${SRCdir}/b2 -j${THREAD_NUM} --build-type=complete \
+	--with-atomic \
+	--with-chrono \
+	--with-container \
+	--with-context \
+	--with-coroutine \
+	--with-coroutine2 \
+	--with-date_time \
+	--with-exception \
+	--with-filesystem \
+	--with-graph \
+	--with-graph_parallel \
+	--with-iostreams \
+	--with-locale \
+	--with-log \
+	--with-math \
+	--with-mpi \
+	--with-program_options \
+	--with-python \
+	--with-random \
+	--with-regex \
+	--with-serialization \
+	--with-signals \
+	--with-system \
+	--with-test \
+	--with-thread \
+	--with-timer \
+	--with-type_erasure \
+	--with-wave \
+	--layout=tagged  \
+	--prefix=${PREFIX} install
 
 
-### LINK ###
-source ${SCRIPTPATH}/link
+
--- install_siox.sh
+++ install_siox.sh
@@ -1,32 +1,58 @@
 #!/bin/bash
-
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname ${SCRIPT}`
+
+module purge
+module load betke/glib/2.48.0
+module load betke/openmpi/1.10.2
+module load betke/boost/1_60_0
+module load betke/hdf5/1.8.16
+module load betke/netcdf/4.4.0
 
 ${SCRIPTPATH}/precheck
 
 NAME="siox"
-VERSION="dev"
-
+VERSION="git"
+source ${SCRIPTPATH}/config
+VERSION="$VERSION-${DATE}"
 source ${SCRIPTPATH}/config
 
 
 ### FETCH ###
-if [ ! -d "${SRCdir}" ] 
-then
+#if [ ! -d "${SRCdir}" ] 
+#then
 	cd ${CACHEdir}
-	git clone https://github.com/JulianKunkel/siox.git ${SRCdir} &>>${LOG} &>> "${LOG}"
-else
-	echo "WARNING: ${NAME}-${VERSION} is already downloaded"
-fi
+	git clone https://github.com/JulianKunkel/siox.git ${SRCdir} 
+#else
+	#echo "WARNING: ${NAME}-${VERSION} is already downloaded"
+#fi
 
-#### COMPILE AND INSTALL ###
-${SRCdir}/configure --with-libpq=${INSTALL_ROOT_DIR}/postgresql --with-glib=${INSTALL_ROOT_DIR}/glib  --with-boost=${INSTALL_ROOT_DIR}/boost --prefix=${PREFIX} --build-dir=${BLDdir} --with-likwid=${INSTALL_ROOT_DIR}/likwid --build-wrappers &>> ${LOG}
+### COMPILE AND INSTALL ###
+${SRCdir}/configure \
+	--with-libpq=${INSTALL_ROOT_DIR}/postgresql/9.1.15 \
+	--with-glib=${INSTALL_ROOT_DIR}/glib/2.48.0  \
+	--with-boost=${INSTALL_ROOT_DIR}/boost/1_60_0 \
+	--prefix=${PREFIX} \
+	--build-dir=${BLDdir} \
+	--with-likwid=${INSTALL_ROOT_DIR}/likwid/3.1.3
 
-cd "${BLDdir}" &>> "${LOG}"
-make -j${THREAD_NUM} &>> "${LOG}"
-make install &>> "${LOG}"
+cd "${BLDdir}"
+make -j${THREAD_NUM}
+make install
 
+## INSTALL WRAPPERS ###
+cd $SRCdir/tools/siox-skeleton-builder/layers/posix/
+./waf configure --siox=${PREFIX}  --prefix=${PREFIX}
+./waf install
 
-### LINK ###
-source ${SCRIPTPATH}/link
+cd $SRCdir/tools/siox-skeleton-builder/layers/mpi/
+./waf configure --siox=${PREFIX}  --prefix=${PREFIX} --mpi-const
+./waf install
+
+cd $SRCdir/tools/siox-skeleton-builder/layers/hdf5/
+./waf configure --siox=${PREFIX}  --prefix=${PREFIX} --hdf5="${INSTALL_ROOT_DIR}/hdf5/1.8.16"
+./waf install
+
+cd $SRCdir/tools/siox-skeleton-builder/layers/netcdf4/
+./waf configure --siox=${PREFIX}  --prefix=${PREFIX} --netcdf=="${INSTALL_ROOT_DIR}/netcdf/4.4.0"
+./waf install
